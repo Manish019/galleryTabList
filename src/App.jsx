5fac341ce18030252ba6dbc4d898f5c851cc570e
Manish Aryan <arayanmanish@gmail.com>
2025-10-24 21:33:27 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,16 @@ import GalleryCard from './components/GalleryCard'
 const App = () => {
   return (
     <div className="bg-[#363C43] p-4 sm:p-6 lg:p-8 min-h-screen  items-center justify-center">
+    <h1 className='text-2xl text-white text-center mb-5 uppercase'>Assignment</h1>
+
+
         <TabContent />
-      <div className="w-full max-w-[670px] mx-auto mt-5">   
-        <GalleryCard />
-      </div>
+      <div className='w-full max-w-[670px] mx-auto mt-5'>
+          <hr className="border-t border-3 border-white/10 rounded-full mt-10 mb-10"/>
+        </div>
+        <div className="w-full max-w-[670px] mx-auto mt-5">
+          <GalleryCard />
+        </div>
    </div>
 
   
--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -4,12 +4,12 @@ import './tabList.css'
 const Tabs = ({ activeTab, setActiveTab }) => {
   const tabs = ["About Me", "Experiences", "Recommended"];
   return (
-    <div className="flex gap-2 p-3 pb-3 overflow-x-auto no-scrollbar bg-black/80 border-b border-slate-700/30 m-3 border rounded-2xl">
+    <div className="flex gap-2 p-2 pb-3 overflow-x-auto no-scrollbar bg-black/80 border-b border-slate-700/30 m-3 border rounded-2xl text-2xl">
       {tabs.map((tab) => (
         <button
           key={tab}
           onClick={() => setActiveTab(tab)}
-          className={`px-4 py-1 rounded-2xl text-base ${activeTab === tab
+          className={`px-0 md:px-4 lg:px-8 py-1 rounded-2xl text-base ${activeTab === tab
             ? "bg-[#28292F] text-white custom-1 custom-2 tabbutton"
             : " text-[#969696]"
             }`}
