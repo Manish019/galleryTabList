aa4f52c576e09c87fd696f95424935ba970c1fad
Manish Aryan <arayanmanish@gmail.com>
2025-10-24 22:08:51 +0530

    responsive

--- src/components/TabList.jsx
+++ src/components/TabList.jsx
@@ -4,20 +4,21 @@ import './tabList.css'
 const Tabs = ({ activeTab, setActiveTab }) => {
   const tabs = ["About Me", "Experiences", "Recommended"];
   return (
-    <div className="flex gap-2 p-2 pb-3 overflow-x-auto no-scrollbar bg-black/80 border-b border-slate-700/30 m-3 border rounded-2xl text-2xl">
+    <div className="flex  p-2 pb-3 overflow-x-auto no-scrollbar bg-black/80 border-b border-slate-700/30 m-3 rounded-2xl text-sm sm:text-base  lg:gap-5 gap-2">
       {tabs.map((tab) => (
         <button
           key={tab}
           onClick={() => setActiveTab(tab)}
-          className={`px-0 md:px-4 lg:px-8 py-1 rounded-2xl text-base ${activeTab === tab
-            ? "bg-[#28292F] text-white custom-1 custom-2 tabbutton"
-            : " text-[#969696]"
+          className={`py-2 rounded-2xl custom-1 custom-2 tabbutton transition-colors duration-300 ${activeTab === tab
+              ? "bg-[#28292F] text-white px-2"
+              : "text-[#969696] hover:text-white"
             }`}
         >
           {tab}
         </button>
       ))}
     </div>
+
   );
 };
 
